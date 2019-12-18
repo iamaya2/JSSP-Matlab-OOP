baeb65e5dd9ba2451512a848d2766e7e310bba34
iamaya2 <iamaya2@gmail.com>
2019-12-18 11:34:59 -0600

    changes

--- ExampleUPSO.m
+++ ExampleUPSO.m
@@ -20,11 +20,12 @@ properties = struct('visualMode', true, 'verboseMode', true, ...
 nbJobs = 6;%3; % Number of jobs desired in the instance
 nbMachines = 6;%4; % Number of machines desired in the instance
 timeRanges = [0 10]; % Min,Max processing times for the instance
-fh = @(x)EvaluateUPSOtoJSSP(x, nbJobs);
+fh = @(x)EvaluateUPSOtoJSSP(x, nbJobs); % Objective function for UPSO
 flim = [repmat(timeRanges,nbJobs*nbMachines,1); repmat([0.01 nbMachines],nbJobs*nbMachines,1)]; % First processing times, then machine IDs
-properties = struct('visualMode', true, 'verboseMode', true, ...
-                'populationSize', 15, 'maxIter', 100, 'maxStagIter', inf, ...
-                'selfConf', 2, 'globalConf', 2.5);
+% UPSO properties definition
+properties = struct('visualMode', false, 'verboseMode', true, ...
+                'populationSize', 15, 'maxIter', 100, 'maxStagIter', 100, ...
+                'selfConf', 2, 'globalConf', 2.5, 'unifyFactor', 0.25);
 % Call to the optimizer           
 [position,fitness,details] = UPSO2(fh, flim, properties);
 generatedInstance = UPSOtoJSSP(position,nbJobs);
