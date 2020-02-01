2893c94839143fab643b8530121f451b070bfc28
iamaya2 <iamaya2@gmail.com>
2020-01-31 19:35:25 -0600

    Update TaskManagerParameters.m

--- TaskManagerParameters.m
+++ TaskManagerParameters.m
@@ -9,4 +9,4 @@ nbRep = 30; %Number of instances per combination
 heurID = [2 1]; %Number: 1.LPT 2.SPT 3.MPA 4.LPA ; first the heuristic to worsen, second the heuristic to improve
 heuristicID = ["SPT" "LPT"]; % 3 Letters code: 1.LPT 2.SPT 3.MPA 4.LPA ; first the heuristic to worsen, second the heuristic to improve
 timeRanges = [0 10]; % Min,Max processing times for the instance
-TaskMaganerPrueba(nbJobsV,nbMachinesV,timeRanges,population,selfconf, globalconf, unifyfactor, nbRep, heurID, heuristicID);
+TaskManagerPrueba(nbJobsV,nbMachinesV,timeRanges,population,selfconf, globalconf, unifyfactor, nbRep, heurID, heuristicID);
