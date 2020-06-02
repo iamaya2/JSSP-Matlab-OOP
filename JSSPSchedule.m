03e31c62722f44183925d6cd19fe59fba7852e20
iamaya2 <iamaya2@gmail.com>
2020-06-02 11:10:47 -0500

    Update JSSPSchedule.m

--- JSSPSchedule.m
+++ JSSPSchedule.m
@@ -157,7 +157,7 @@ classdef JSSPSchedule < handle  % Only one schedule should be around
             hold on            
             for idM = 1 : obj.nbMachines %length(obj.schedule)
                 eachMachine = obj.schedule(idM);
-                if ~isempty(eachMachine.makespan)
+                if ~isempty(eachMachine.jobList)
                     for idx = 1 : length([eachMachine.activities])
                         eachActivity = eachMachine.activities(idx);
                         boxwidth = eachActivity.processingTime;
